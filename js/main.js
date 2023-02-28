7bf7ea786b6547daca7108ae9e0109d3083d0ff1
Sensha <36817319+JonatanNylund@users.noreply.github.com>
2023-02-28 09:14:27 +0100

    commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function randomWord() {
+    let ranObj = wordList[Math.floor(Math.random() * wordList.lenght)];
+    let word = ranObj.word;
+    console.log(word);
+
+    let html = "";
+    for (let i = 0; i < word.length; i++){
+        const element = Array[i];
+    }
+}
+
+randomWord();
