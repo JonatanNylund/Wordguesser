ceedfce0c2fc6e67e0732c5d3432c554de2c8384
Sensha <36817319+JonatanNylund@users.noreply.github.com>
2023-02-28 12:19:37 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
+const inputs = document.querySelector(".inputs"),
+resetBtn = document.querySelector(".reset-btn"),
+hint = document.querySelector(".hint span");
+
+
 function randomWord() {
     let ranObj = wordList[Math.floor(Math.random() * wordList.lenght)];
     let word = ranObj.word;
     console.log(word);
 
+    hint.innerText = ranObj.hint;
+
     let html = "";
     for (let i = 0; i < word.length; i++){
-        const element = Array[i];
+        html += `<input type="text" disabled>`;
     }
+    inputs.innerHTML = html;
 }
 
 randomWord();
+
+resetBtn.addEventListener("click", randomWord);
